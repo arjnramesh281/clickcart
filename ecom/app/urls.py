2f1a6e3164b78ea76f7f6f990546e146637c2248
arjnramesh281 <arjunramesh281@gmail.com>
2024-11-14 14:00:41 +0530

    clickcart

--- ecom/app/urls.py
+++ ecom/app/urls.py
@@ -4,5 +4,6 @@ from . import views
 urlpatterns=[
     path("",views.log),
     path("admin_home",views.admin_home),
+    path("logout",views.admin_logout),
     path("registration",views.reg),
 ]
--- ecom/app/views.py
+++ ecom/app/views.py
@@ -4,23 +4,36 @@ from django.contrib import messages
 
 # Create your views here.
 def log(req):
+    if 'admin' in req.session:
+        return redirect(admin_home)
     if req.method=='POST':
         username=req.POST['username']
         password=req.POST['password']
         data=authenticate(username=username,password=password)
         if data:
             login(req,data)
+            req.session['admin']=username
             return redirect(admin_home)
         else:
+            messages.warning(req, " Invalid username or password.")
             return redirect(log)
     else:
-        messages.warning(req, " Invalid username and password.")
         return render(req,'login.html')
-    
+
+# admin logout
+
+def admin_logout(req):
+    logout(req)
+    req.session.flush()
+    return redirect(log)
+
 # admin home 
     
 def admin_home(req):
-    return render(req,'admin/home.html')
+    if 'admin' in req.session:
+        return render(req,'admin/home.html')
+    else:
+        return redirect(log)
 
 # registration page
 
